9a17b4647e3835b5df9fcd58b00ff394aaccf18d
Daria H <dholubenna@gamil.com>
2025-08-20 11:44:29 +0300

    its funny

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -3,22 +3,21 @@ import {
   HydrationBoundary,
   dehydrate,
 } from '@tanstack/react-query';
-import { fetchNotes, TagType} from '@/lib/api';
+import { fetchNotes, TagType } from '@/lib/api';
 import NotesClient from './Notes.client';
 
-
-type PageProps = {
+interface PageProps {
   params: {
     slug?: string[];
   };
-};
+}
 
 const NotesByTags = async ({ params }: PageProps) => {
   const queryClient = new QueryClient();
   const page = 1;
   const perPage = 12;
-  
-  const tag = params?.slug?.[0] === 'all' ? undefined : (params?.slug?.[0] as TagType);
+
+  const tag = params?.slug?.[0] === 'all' ? undefined : (params?.slug?.[0] as TagType | undefined);
 
   await queryClient.prefetchQuery({
     queryKey: ['notes', '', page, perPage, tag],
